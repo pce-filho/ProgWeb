35588d6c50ea7d2772465d90ce9b917cd6aaace4
Paulo Cezar de Queiroz Hermida Filho <pcqhf@icomp.ufam.edu.br>
2017-04-20 16:33:37 -0400

    exec pw

---
+++ js1/script.js
@@ -0,0 +1,15 @@
+for (i = 1; i <= 10; i++){
+  document.writeln("<table>");
+  document.writeln("<th colspan=2> Produtos de " + i + "</th>");
+  for (j = 1; j <= 10; j++){
+    document.writeln("<tr>");
+    document.writeln("<td>");
+    document.writeln(i + "x" + j);
+    document.writeln("</td>");
+    document.writeln("<td>");
+    document.writeln(i * j);
+    document.writeln("</td>");
+    document.writeln("</tr>")
+  }
+  document.writeln("</table>");
+}
